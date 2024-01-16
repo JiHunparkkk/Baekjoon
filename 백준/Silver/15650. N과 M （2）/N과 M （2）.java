2ab7ef30d24ea2ae722ee4e3a4d5014bd4e297f5
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-01-16 22:39:00 +0900

    [Silver III] Title: N과 M (2), Time: 168 ms, Memory: 16704 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static int[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        visited = new int[n];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                if (i < m - 1 && arr[i] > arr[i + 1]) {
+                    return;
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                arr[depth] = i + 1;
+                dfs(depth + 1);
+                visited[i] = 0;
+            }
+        }
+    }
+}
