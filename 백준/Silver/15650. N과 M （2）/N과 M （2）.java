35f0c9004a6ca8bd777f97b12881988153178136
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-01-31 11:13:36 +0900

    [Silver III] Title: N과 M (2), Time: 144 ms, Memory: 15716 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,41 +4,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static int n, m;
-    static int[] arr;
-    static int[] visited;
-    static StringBuilder sb = new StringBuilder();
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        arr = new int[n];
-        visited = new int[n];
-
-        dfs(0, 0);
-        System.out.println(sb);
-    }
-
-    private static void dfs(int depth, int start) {
-        if (depth == m) {
-            for (int i = 0; i < m; i++) {
-                sb.append(arr[i] + " ");
-            }
-            sb.append("\n");
-            return;
-        }
-
-        for (int i = start; i < n; i++) {
-            if (visited[i] == 0) {
-                visited[i] = 1;
-                arr[depth] = i + 1;
-                dfs(depth + 1, i + 1);
-                visited[i] = 0;
-            }
-        }
-    }
+	
+	private static int n,m;
+	private static int[] arr;
+	private static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());	
+		arr = new int[m];
+        
+		solution(0, 1);
+		
+		System.out.println(sb);
+		br.close();
+	}
+	
+	private static void solution(int cnt,int start) {
+		if(cnt==m) {
+			for(int x : arr) {
+				sb.append(x+" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start;i<=n;i++) {
+			arr[cnt] = i;
+			solution(cnt+1, i+1);
+		}
+	}
 }
