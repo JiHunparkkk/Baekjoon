f902876faaad2bb8b71c44af61d5fd7b26473049
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-02 17:43:34 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45492 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -4,22 +4,24 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
+	public static void main(String[] args) throws IOException {
+		  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
+	        int n = Integer.parseInt(br.readLine());
 
-        Queue<Integer> queue = new LinkedList<>();
+	        Queue<Integer> queue = new LinkedList<>();
 
-        for (int i = 1; i <= n; i++) {
-            queue.offer(i);
-        }
-        while (queue.size() != 1) {
-            queue.poll();
-            Integer poll = queue.poll();
-            queue.offer(poll);
-        }
-        System.out.println(queue.poll());
-    }
+	        for (int i = 1; i <= n; i++) {
+	            queue.offer(i);	//1부터 N까지 번호 초기화
+	        }
+	       
+	        while (queue.size() != 1) {	//카드가 하나 남을 때 까지 반복
+	            queue.poll();	//최상단 카드 버림
+	            Integer poll = queue.poll();	//그 다음 카드를 빼서
+	            queue.offer(poll);				//제일 아래 카드 밑으로 옮김
+	        }
+	        
+	        System.out.println(queue.poll());	//마지막 카드를 출력
+	}
 }
