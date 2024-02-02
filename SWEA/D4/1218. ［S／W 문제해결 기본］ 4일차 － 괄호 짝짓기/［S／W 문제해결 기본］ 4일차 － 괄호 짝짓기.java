f7dce3dac0025f31eed6eeac6ed7a76db7832bc7
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-02 11:29:21 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 129 ms, Memory: 20,632 KB -BaekjoonHub

---
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		for(int test_case = 1; test_case <= 10; test_case++)
+		{
+			int n = sc.nextInt();
+			String input = sc.next();
+			int answer = 1;
+			Stack<Character> stack = new Stack<>();
+			
+			for(int i=0;i<input.length();i++) {
+				char op = input.charAt(i);
+				
+				if(isOpen(op)) {
+					stack.push(op);
+				}else {
+					if(!Matching(stack.pop(), op)) {
+						answer = 0;
+						break;
+					}
+				}
+			}
+			
+			System.out.println("#"+test_case+" "+answer);
+		}
+	}
+	
+	private static boolean isOpen(char op) {
+		return op == '(' || op == '['  || op == '{' || op == '<';
+	}
+	
+	private static boolean Matching(char open, char close) {
+		switch(open) {
+		case '(':
+			return close==')';
+		case '[':
+			return close==']';
+		case '{':
+			return close=='}';
+		case '<':
+			return close=='>';
+		}
+		return true;
+	}
+}
