4c5999ec5d781c13b0d27d71e443ece8588f80b6
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-05 13:12:58 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 332 ms, Memory: 19660 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,41 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-// Queue를 돌면서 해당 위치의 숫자를 poll
 public class Main {
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		int n,k;
+		n = sc.nextInt();
+		k = sc.nextInt();
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		for(int i=1;i<=n;i++) {
+			queue.offer(i);
+		}
+		
+		sb.append("<");
+		while(!queue.isEmpty()) {
+			for(int i=0;i<k-1;i++) {
+				queue.offer(queue.poll());
+			}
+			if(queue.size()==1) {
+				break;
+			}
+			sb.append(queue.poll()).append(", ");
+		}
+		sb.append(queue.poll()).append(">");
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        Queue<Integer> queue = new LinkedList();
-
-        int n,k;
-
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-
-        // n개 만큼 queue에 값 add
-        for(int i=1;i<=n;i++){
-            queue.add(i);
-        }
-
-        sb.append("<");
-        // queue가 비어있을 때까지 poll
-        while(n-->0){
-            int m=k;
-            while(--m>0){
-                queue.add(queue.poll());
-            }// while
-            sb.append(queue.poll());
-            if(n!=0)
-                sb.append(", ");
-        }// while
-        sb.append(">");
-        System.out.println(sb);
-    }// main
+		System.out.println(sb);
+	}
 }
