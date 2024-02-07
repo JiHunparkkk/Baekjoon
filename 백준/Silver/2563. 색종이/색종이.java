b1b9efb7ae4c2c6bcafbe52e4579b158880f2b5a
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-07 16:43:12 +0900

    [Silver V] Title: 색종이, Time: 128 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,32 +1,54 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n,answer=0;
-
-        n = sc.nextInt();
-        int[][] x = new int[n][2];
-        int[][] total = new int[100][100];
-
-        for (int i = 0; i < n; i++) {
-            x[i][0] = sc.nextInt();
-            x[i][1] = sc.nextInt();
-            for (int j = x[i][0]; j < x[i][0] + 10; j++) {
-                for (int k = x[i][1]; k < x[i][1]+10; k++) {
-                    total[j][k]=1;
-                }
-            }
-        }
-
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if(total[i][j]==1)
-                    answer++;
-            }
-        }
-
-        System.out.println(answer);
-    }
+	
+	private static int[][] board = new int[101][101];
+	private static int[] dx = {-1,0,1,0};
+	private static int[] dy = {0,1,0,-1};
+	private static int answer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=0;i<n;i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			for(int j=x;j<x+10;j++) {
+				for(int k=y;k<y+10;k++) {
+					board[j][k] = 1;
+				}
+			}
+		}
+		
+		for(int i=0;i<100;i++) {
+			for(int j=0;j<100;j++) {
+				if(board[i][j]==1) {
+					dfs(i, j);
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+	
+	private static void dfs(int x, int y) {
+		for(int i=0;i<4;i++) {
+			int nx = x+ dx[i];
+			int ny = y+ dy[i];
+			
+			if(nx>=0 && ny>=0 && nx<100 && ny<100 && board[nx][ny]==1) {
+				board[nx][ny]=0;
+				answer++;
+				dfs(nx, ny);
+			}
+		}
+	}
 }
