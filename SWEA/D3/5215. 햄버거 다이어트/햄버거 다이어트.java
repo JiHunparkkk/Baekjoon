389d8da5623a00c3da180787d74c539aba980778
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-08 12:13:30 +0900

    [D3] Title: 햄버거 다이어트, Time: 203 ms, Memory: 23,568 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,66 +1,51 @@
 import java.util.Scanner;
-
-public class Solution {
-	
-	private static int N;
-	private static long L,answer;
-	private static Taste[] arr;
-	private static boolean[] visited;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			
-			N = sc.nextInt();
-			L = sc.nextLong();
-			arr = new Taste[N];
-			visited = new boolean[N];
-			
-			for(int i=0;i<N;i++) {
-				arr[i] = new Taste(sc.nextInt(), sc.nextInt());
-			}
-			answer = 0;
-			solution(0);
-			
-			System.out.println("#"+test_case+" "+answer);
-		}
-	}
-	
-	private static void solution(int start) {
-		if(start==N) {
-			int cal = 0;
-			int score = 0;
-			for(int i=0;i<N;i++) {
-				if(visited[i]) {
-					cal+=arr[i].cal;
-					score +=arr[i].score;
-				}
-			}
-			if(cal<=L) {
-				answer = Math.max(answer, score);
-			}
-			return;
-		}
-		
-		visited[start] = true;
-		solution(start+1);
-		visited[start] = false;
-		solution(start+1);
-	}
-	
-
+ 
+public class Solution{
+     
+    private static int N,L,answer;
+    private static Taste[] arr;
+     
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+         
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+             
+            N = sc.nextInt();
+            L = sc.nextInt();
+            arr = new Taste[N];
+             
+            for(int i=0;i<N;i++) {
+                arr[i] = new Taste(sc.nextInt(), sc.nextInt());
+            }
+            answer=0;
+            solution(0,0,0,0);
+             
+            System.out.println("#"+test_case+" "+answer);
+        }
+    }
+     
+    private static void solution(int start,int flag,int calories,int score) {
+        if(calories<=L) {
+            answer = Math.max(answer, score);
+        }
+         
+        for(int i=start;i<N;i++) {
+            if((flag & 1<<i) == 0) {
+                solution(i+1, flag | 1<<i, calories+arr[i].cal, score+arr[i].score);
+            }
+        }
+    }
 }
-
+ 
 class Taste{
-	int score;
-	int cal;
-	
-	public Taste(int score, int cal) {
-		this.score = score;
-		this.cal = cal;
-	}
+    int score;
+    int cal;
+     
+    public Taste(int score, int cal) {
+        this.score = score;
+        this.cal = cal;
+    }
 }
