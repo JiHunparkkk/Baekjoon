a4e3b92a876c3ed96f030e2daa9f11671bd47ee9
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-13 16:43:31 +0900

    [Silver IV] Title: 설탕 배달, Time: 212 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,29 +1,34 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int answer = 0;
-
-        while (n > 0) {
-            if (n < 3) {
-                answer = -1;
-                break;
-            }
-            if (n % 5 == 0 || n % 5 == 3) {
-                answer += n / 5;
-                n %= 5;
-                answer += n / 3;
-                n %= 3;
-            } else {
-                answer++;
-                n -= 3;
-            }
-
-        }
-
-        System.out.println(answer);
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int[] dp = new int[n+3];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		
+		dp[3] = 1;
+		dp[4] = -1;
+		dp[5] = 1;
+		for(int i=6;i<=n;i++) {
+			int min = dp[i];
+			if(dp[i-3]!=-1 && min>dp[i-3]) {
+				min = dp[i-3];
+			}
+			if(dp[i-5]!=-1 && min>dp[i-5]) {
+				min = dp[i-5];
+			}
+			
+			if(min<dp[i]) {
+				dp[i] = min+1;
+			}
+		}
+		
+		if(dp[n]==Integer.MAX_VALUE) {
+			dp[n] = -1;
+		}
+		System.out.println(dp[n]);
+	}
 }
