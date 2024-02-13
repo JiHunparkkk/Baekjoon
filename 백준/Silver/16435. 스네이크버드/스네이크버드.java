e29f96b4161188bfdbe7d05c05531aa73e677bad
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-13 12:12:31 +0900

    [Silver V] Title: 스네이크버드, Time: 136 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n,l;
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		l = Integer.parseInt(st.nextToken());
+		
+		int[] fruits = new int[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			fruits[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(fruits);
+		for(int i=0;i<n;i++) {
+			if(fruits[i]>l) break;
+			l++;
+		}
+		
+		System.out.println(l);
+	}
+}
