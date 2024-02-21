6d2b6d20b0d055a0049ab0be0979f436abdc0eb4
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-21 19:56:20 +0900

    [Gold V] Title: 암호 만들기, Time: 200 ms, Memory: 18508 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,59 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int L, C;
-    static char[] arr;
-    static ArrayList<String> list = new ArrayList<>();
+
+    private static int l, c;
+    private static char[] code;
+    private static final StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        st = new StringTokenizer(br.readLine()," ");
-        L = Integer.parseInt(st.nextToken());
-        C = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        l = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        code = new char[c];
 
-        st = new StringTokenizer(br.readLine()," ");
-        arr = new char[C];
-        for (int i = 0; i < C; i++) {
-            arr[i] = st.nextToken().charAt(0);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < c; i++) {
+            code[i] = st.nextToken().toCharArray()[0];
         }
-        Arrays.sort(arr);
 
-        DFS(0, "");
-        for (String s : list) {
-            System.out.println(s);
-        }
+        Arrays.sort(code);
+        dfs(0, "");
+
+        System.out.println(sb);
     }
 
-    public static void DFS(int index, String str) {
-        if(str.length()==L){
-            if (check(str)) {
-                list.add(str);
-                return;
+    private static void dfs(int start, String result) {
+        if (result.length() == l) {
+            if (isValidCode(result)) {
+                sb.append(result).append("\n");
+            }
+            return;
+        }
+
+        if (start >= c) {
+            return;
+        }
+
+        dfs(start + 1, result + code[start]);
+        dfs(start + 1, result);
+    }
+
+    private static boolean isValidCode(String result) {
+        int moemCnt = 0;
+        int jaemCnt = 0;
+
+        for (char ch : result.toCharArray()) {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                moemCnt++;
+            } else {
+                jaemCnt++;
             }
         }
 
-        if(index>=C) return;
-
-        DFS(index + 1, str + arr[index]);
-        DFS(index + 1, str);
-    }
-
-    public static boolean check(String str) {
-        int mo = 0, za = 0;
-
-        for (char ch : str.toCharArray()) {
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
-                mo++;
-            else
-                za++;
-        }
-        return (mo>=1 && za>=2);
+        return jaemCnt >= 2 && moemCnt >= 1;
     }
 }
+
