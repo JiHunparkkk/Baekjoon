86c641aa8223fbfba8bc30df6b9ed1e562a170ac
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-27 15:29:59 +0900

    [Silver III] Title: 1로 만들기, Time: 232 ms, Memory: 21600 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,19 +1,22 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		
-		int n = scanner.nextInt();		
+		int n = sc.nextInt();
 		int[] dp = new int[n+3];
-		Arrays.fill(dp, Integer.MAX_VALUE);
 		
+		Arrays.fill(dp, Integer.MAX_VALUE);
 		dp[1] = 0;
 		dp[2] = 1;
-		dp[3] = 1;
+		dp[3] = 1;			
+		
+		
 		for(int i=4;i<=n;i++) {
 			int min = dp[i];
+			
 			if(min>dp[i-1]) {
 				min = dp[i-1];
 			}
@@ -23,7 +26,7 @@ public class Main{
 			if(i%3==0 && min>dp[i/3]) {
 				min = dp[i/3];
 			}
-			dp[i] = min+1;
+			dp[i] = min + 1;
 		}
 		
 		System.out.println(dp[n]);
