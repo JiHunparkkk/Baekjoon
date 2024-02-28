8e7a8c70a4bd8220f261e64f45fd1b766ad09bc7
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-02-28 13:54:17 +0900

    [Gold IV] Title: 파이프 옮기기 2, Time: 132 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
+++ 백준/Gold/17069. 파이프 옮기기 2/파이프 옮기기 2.java
@@ -1,53 +1,44 @@
-import java.awt.desktop.OpenURIHandler;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static final int BLOCK = 1;
-	private static final int ROW = 2;
-	private static final int COL = 3;
-	private static final int DIAGONAL = 4;
-
-	public static void main(String[] args) throws Exception {
+		
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-	
-		int n = Integer.parseInt(br.readLine());
-		int[][] board = new int[n+1][n+1];
 		
-		for(int i=1;i<=n;i++) {
+		int n = Integer.parseInt(br.readLine());
+		int[][] board = new int[n][n];
+		for(int i=0;i<n;i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=1;j<=n;j++) {
+			for(int j=0;j<n;j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
+		System.out.println(solution(n,board));
+	}
+	
+	private static long solution(int n,int[][] board) {
+		long[][][] dp = new long[n][n][3];
+		dp[0][1][0] = 1;
 		
-		long[][][] dp = new long[n+1][n+1][5];
-		
-		dp[1][2][ROW] = 1;
-		for(int i=1;i<=n;i++) {
-			for(int j=3;j<=n;j++) {
-				if(board[i][j]==BLOCK) {
-					continue;
+		for(int i=0;i<n;i++) {
+			for(int j=2;j<n;j++) {
+				if(board[i][j] == 0) {	//가로
+					dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];					
 				}
-				
-				//가로
-				dp[i][j][ROW] = dp[i][j-1][ROW] + dp[i][j-1][DIAGONAL];
-				
-				//세로
-				dp[i][j][COL] = dp[i-1][j][COL] + dp[i-1][j][DIAGONAL];
-				
-				//대각선
-				if(board[i-1][j]==0 && board[i][j-1]==0) {
-					dp[i][j][DIAGONAL] = dp[i-1][j-1][DIAGONAL] + dp[i-1][j-1][COL] + dp[i-1][j-1][ROW];
+				if(i-1>=0 && board[i][j]==0) {	//세로
+					dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+				}
+				if(i-1>=0 && board[i][j]==0 && board[i][j-1]==0 && board[i-1][j]==0) {
+					dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
 				}
 			}
 		}
 		
-		System.out.println(dp[n][n][COL] + dp[n][n][ROW] + dp[n][n][DIAGONAL]);
+		return dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2];
 	}
 }
