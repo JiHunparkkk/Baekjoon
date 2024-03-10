71a48414fc63e3ce43bf8e0a760284bff05e1197
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-03-10 16:11:39 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 504 ms, Memory: 112696 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n, m;
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            union(a, b);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            find(i);
+        }
+        long answer = Arrays.stream(parent).distinct().count() - 1; //0인덱스 갯수 제거(-1)
+        System.out.println(answer);
+    }
+
+    private static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+
+        if (aRoot == bRoot) {
+            return;
+        }
+
+        parent[bRoot] = aRoot;
+    }
+}
