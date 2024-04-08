0338f5ed0b1df70b494481ef3c5d64f6febb5022
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-04-08 20:50:17 +0900

    [Gold II] Title: 색종이 붙이기, Time: 244 ms, Memory: 23068 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -1,96 +1,80 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
- 
+
 public class Main {
-    static int[][] map;
-    static int[] paper = { 0, 5, 5, 5, 5, 5 };
-    static int ans = Integer.MAX_VALUE;
- 
-    public static void main(String[] args) throws Exception {
+
+    private static final int LEN = 10;
+    private static int[][] arr;
+    private static int[] paper = {0, 5, 5, 5, 5, 5};
+    private static int answer;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
- 
-        map = new int[10][10];
-        for (int i = 0; i < map.length; i++) {
+
+        arr = new int[LEN][LEN];
+        for (int i = 0; i < LEN; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < map[i].length; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < LEN; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
- 
-        DFS(0, 0, 0);
- 
-        if (ans == Integer.MAX_VALUE) {
-            ans = -1;
+
+        answer = Integer.MAX_VALUE;
+        dfs(0, 0);
+        if (answer == Integer.MAX_VALUE) {
+            answer = -1;
         }
- 
-        bw.write(ans + "\n");
-        bw.close();
-        br.close();
+        System.out.println(answer);
     }
-    
-    // DFS + 백트래킹
-    public static void DFS(int x, int y, int cnt) {
-        // 맨 끝점에 도달하였을 경우, ans와 cnt 비교하고 종료.
-        if (x >= 9 && y > 9) {
-            ans = Math.min(ans, cnt);
+
+    private static void dfs(int idx, int cnt) {
+        //0,0 ~ 10, 10
+        if (idx == LEN * LEN) {
+            answer = Math.min(answer, cnt);
             return;
         }
- 
-        // 최솟값을 구해야하는데 ans보다 cnt가 커지는 순간
-        // 더 이상 탐색할 필요가 없어짐.
-        if (ans <= cnt) {
+
+        if (cnt >= answer) {
             return;
         }
- 
-        // 아래 줄로 이동.
-        if (y > 9) {
-            DFS(x + 1, 0, cnt);
-            return;
-        }
- 
-        if (map[x][y] == 1) {
-            for (int i = 5; i >= 1; i--) {
-                if (paper[i] > 0 && isAttach(x, y, i)) {
-                    attach(x, y, i, 0); // 색종이를 붙임.
-                    paper[i]--;
-                    DFS(x, y + 1, cnt + 1);
-                    attach(x, y, i, 1); // 색종이를 다시 뗌.
-                    paper[i]++;
+
+        int x = idx / LEN;
+        int y = idx % LEN;
+
+        if (arr[x][y] == 1) {
+            for (int i = 5; i > 0; i--) {
+                if (paper[i] > 0 && validSquare(x, y, i)) {
+                    paper[i] -= 1;
+                    fill(x, y, i, 0);
+                    dfs(idx + 1, cnt + 1);
+                    fill(x, y, i, 1);
+                    paper[i] += 1;
                 }
             }
-        } else { // 오른쪽으로 이동.
-            DFS(x, y + 1, cnt);
+        } else {
+            dfs(idx + 1, cnt);
         }
     }
- 
-    // 색종이를 붙이는 함수.
-    public static void attach(int x, int y, int size, int state) {
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                map[i][j] = state;
-            }
-        }
-    }
- 
-    // 색종이를 붙일 수 있는지 확인.
-    public static boolean isAttach(int x, int y, int size) {
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                if (i < 0 || i >= 10 || j < 0 || j >= 10) {
-                    return false;
-                }
- 
-                if (map[i][j] != 1) {
+
+    private static boolean validSquare(int x, int y, int idx) {
+        for (int i = x; i < x + idx; i++) {
+            for (int j = y; j < y + idx; j++) {
+                if (i >= LEN || j >= LEN || arr[i][j] == 0) {
                     return false;
                 }
             }
         }
         return true;
     }
- 
+
+    private static void fill(int x, int y, int idx, int num) {
+        for (int i = x; i < x + idx; i++) {
+            for (int j = y; j < y + idx; j++) {
+                arr[i][j] = num;
+            }
+        }
+    }
 }
