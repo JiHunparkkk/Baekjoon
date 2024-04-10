ee8c60bb65b301b0b112e8ac4397378e4366dfb6
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-04-10 22:19:53 +0900

    [Silver I] Title: 포도주 시식, Time: 140 ms, Memory: 14836 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = arr[1];
+
+        if (n > 1) {
+            dp[2] = dp[1] + arr[2];
+        }
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], Math.max(arr[i - 1] + dp[i - 3], dp[i - 2]) + arr[i]);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
