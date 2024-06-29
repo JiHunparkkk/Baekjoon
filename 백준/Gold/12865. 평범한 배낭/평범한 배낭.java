1f2fea899e55349a0ec5b5883a784ba7e9add888
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-06-29 21:12:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 53736 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,38 +1,44 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N, K;
         st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-
-        int[] weight = new int[N + 1];
-        int[] value = new int[1000001];
-        for (int i = 1; i <= N; i++) {
+        int n, k;
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        Product[] products = new Product[n + 1];
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-            weight[i] = Integer.parseInt(st.nextToken());
-            value[i] = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            products[i] = new Product(w, v);
         }
 
-        int[][] dp = new int[N + 1][K + 1];
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
-                if (j >= weight[i]) {
-                    dp[i][j] = Math.max(dp[i - 1][j], value[i] + dp[i - 1][j - weight[i]]);
-                } else {
+        int[][] dp = new int[n + 1][k + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if (j - products[i].w < 0) {
                     dp[i][j] = dp[i - 1][j];
+                    continue;
                 }
+                dp[i][j] = Math.max(dp[i - 1][j], products[i].v + dp[i - 1][j - products[i].w]);
             }
         }
 
-        System.out.println(dp[N][K]);
+        System.out.println(dp[n][k]);
+    }
+
+    private static class Product{
+        int w, v;
+
+        public Product(int w, int v) {
+            this.w = w;
+            this.v = v;
+        }
     }
 }
