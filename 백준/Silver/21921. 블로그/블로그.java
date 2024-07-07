6e344fc9f9f096b5348fc575055614107f683538
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2024-07-07 23:08:10 +0900

    [Silver III] Title: 블로그, Time: 364 ms, Memory: 35564 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,56 +1,49 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int n, x, max = 0, period_result = 0;
-        n = sc.nextInt();
-        x = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int lp = 0, cnt = 0, sum = 0;
-        for (int i = 0; i < x; i++) {
+        //구간 합을 구한다.
+        //해당 구간 합 보다 크면 갯수 초기화
+        int sum = 0;
+        int cnt = 1;
+        int lp = 0, rp;
+        for (rp = 0; rp < X; rp++) {
+            sum += arr[rp];
+        }
+
+        int max = sum;
+        for (int i = rp; i < N; i++, lp++) {
+            sum -= arr[lp];
             sum += arr[i];
-        }
-        for (int rp = x; rp < n; rp++) {
-            if (max < sum) {
+
+            if (sum > max) {
+                cnt = 1;
                 max = sum;
+            } else if (sum == max) {
+                cnt++;
             }
-            sum += arr[rp];
-            sum -= arr[lp];
-            lp++;
-        }
-        if (max < sum) {
-            max = sum;
         }
 
-        lp = cnt = sum = 0;
-        for (int i = 0; i < x; i++) {
-            sum += arr[i];
-        }
-
-        for (int rp = x; rp < n; rp++) {
-            if (max == sum) {
-                period_result++;
-            }
-            sum += arr[rp];
-            sum -= arr[lp];
-            lp++;
-        }
-        if (max == sum) {
-            period_result++;
-        }
-
-        if(max==0)
+        if (max == 0) {
             System.out.println("SAD");
-        else{
+        } else {
             System.out.println(max);
-            System.out.println(period_result);
+            System.out.println(cnt);
         }
 
     }
