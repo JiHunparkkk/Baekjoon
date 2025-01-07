fe3c3644e39db794e7caf5539c7eaa4e44475b3a
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2025-01-07 15:43:22 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 488 ms, Memory: 96196 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,36 +11,49 @@ class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
-        int[] LIS = new int[n];
 
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        LIS[0] = arr[0];
-        int LIS_length = 1;
+        int answer = solution(n, arr);
+        System.out.println(answer);
+    }
+
+    private static int solution(int n, int[] arr) {
+        //dp만을 이용하면 O(n^2)
+        //이분탐색을 이용하면 O(nlogn), 이분탐색은 정렬된 값에만 정확함. 따라서 dp를 조작
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        int j = 0;
         for (int i = 1; i < n; i++) {
-            int key = arr[i];
-
-            if (LIS[LIS_length - 1] < key) {
-                LIS[LIS_length++] = key;
+            if (arr[i] > dp[j]) {
+                dp[++j] = arr[i];
             } else {
-                int left = 0;
-                int right = LIS_length;
-                while (left < right) {
-                    int mid = (left + right) >>> 1;
-
-                    if (LIS[mid] < key) {
-                        left = mid + 1;
-                    } else {
-                        right = mid;
-                    }
-                }
-                LIS[left] = key;
+                //현재 arr[i] 값보다 큰 값중 제일 작은 값을 이분탐색으로 찾음
+                int index = binarySearch(dp, arr[i], j);
+                dp[index] = arr[i];
             }
         }
 
-        System.out.println(LIS_length);
+        return j + 1;
+    }
+
+    private static int binarySearch(int[] dp, int target, int length) {
+        int left = 0;
+        int right = length;
+
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(dp[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
