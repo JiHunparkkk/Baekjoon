b8d2203e81eab5c4bd63fa08f8889d5ac88a9af9
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2025-04-28 15:08:36 +0900

    [Gold IV] Title: 좋다, Time: 164 ms, Memory: 14744 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -2,56 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static int n;
-	private static int[] arr;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int answer = 0;
-		Arrays.sort(arr);
-		for(int i = 0; i < n; i++) {
-			int target = arr[i];
-			
-			int left = (i == 0 ? i + 1 : 0);
-			int right = n - 1 == i ? n - 2 : n - 1;
-						
-			while(left < right) {
-				if(arr[left] + arr[right] == target) {
-					answer++;
-					break;
-				}
-				
-				if(arr[left] + arr[right] < target) {
-					left++;
-					if(left == i) {
-						left++;
-					}
-				}else {
-					right--;
-					if(right == i) {
-						right--;
-					}
-				}
-			}
-		}
-		
-		System.out.println(answer);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int answer = 0;
+        for(int i = 0; i < N; i++) {
+            int target = arr[i];
+
+            int left = i == 0 ? i + 1 : 0;
+            int right = i == N - 1 ? N - 2 : N - 1;
+            while(left < right) {
+                if(arr[left] + arr[right] == target) {
+                    answer++;
+                    break;
+                }
+
+                if(arr[left] + arr[right] > target) {
+                    right--;
+                    if(right == i) {
+                        right --;
+                    }
+                } else {
+                    left++;
+                    if(left == i) {
+                        left++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
 
 }
