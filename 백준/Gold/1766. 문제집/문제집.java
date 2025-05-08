9c6a08b2254cd244757581e14cd6cec77f47f48e
JiHun Park <44937448+JiHunparkkk@users.noreply.github.com>
2025-05-08 14:47:41 +0900

    [Gold II] Title: 문제집, Time: 424 ms, Memory: 42764 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -8,67 +8,53 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int n, m;
-    private static List<List<Integer>> list;
-    private static int[] indegree;
+    private static int N, M;
+    private static int[] degree;
+    private static List<List<Integer>> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        list = new ArrayList<>();
-        indegree = new int[n + 1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        degree = new int[N + 1];
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i <= N; i++) {
             list.add(new ArrayList<>());
         }
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int first = Integer.parseInt(st.nextToken());
-            int second = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
 
-            indegree[second]++;
-            list.get(first).add(second);
-        }
-        System.out.println(solution());
-    }
-
-    private static String solution() {
-        boolean[] visited = new boolean[n + 1];
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int i = 1; i <= n; i++) {
-            if (indegree[i] == 0) {
-                pq.offer(i);
-            }
+            degree[b]++;
+            list.get(a).add(b);
         }
 
         StringBuilder sb = new StringBuilder();
-        while (!pq.isEmpty()) {
-            int poll = pq.poll();
-            if (!visited[poll] && indegree[poll] == 0) {
-                sb.append(poll).append(" ");
-                visited[poll] = true;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 1; i <= N; i++) {
+            if(degree[i] == 0) {
+                pq.add(i);
             }
+        }
 
-            List<Integer> now = list.get(poll);
-            for (int i = 0; i < now.size(); i++) {
-                int next = now.get(i);
+        while(!pq.isEmpty()) {
+            int poll = pq.poll();
 
-                if(visited[next]) {
-                    continue;
-                }
+            sb.append(poll).append(" ");
 
-                indegree[next]--;
-                if(indegree[next] == 0) {
-                    pq.offer(next);
+            for(int i = 0; i < list.get(poll).size(); i++) {
+                int next = list.get(poll).get(i);
+                degree[next]--;
+                if(degree[next] == 0) {
+                    pq.add(next);
                 }
             }
         }
-        return sb.toString();
+
+        System.out.println(sb);
     }
 }
